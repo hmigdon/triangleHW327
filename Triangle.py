2edc9dde9896cffecc8660c2ea2ac65d256f0f6d
Houston <hmigdon@stevens.edu>
2019-01-28 18:20:35 -0500

    rid of input
    easier to test when the user input in the form of calling

--- Triangle.py
+++ Triangle.py
@@ -1,26 +1,23 @@
-def classify_triangle(a, b, c):
+def classify_triangle(a,b,c):
 #the letters abc represent the three sides of a given triangle
 #The goal of this function is to classify that given triangle into its type
 #Types are
 
-    triangle = str("Triangle Classification")
-
-    a = int(input("a: "))
-    b = int(input("b: "))
-    c = int(input("c: "))
-
     if a == b == c:
-        triangle = str("Equilateral triangle")
+        print("Equilateral Triangle")
     elif a == b or b == c or a == c:
-        triangle = str("Isosceles triangle")
+        print("Isosceles triangle")
     else:
-        triangle = str("Scalene triangle")
-
-    print("Triangle is classified as: " + triangle)
+        print("Scalene triangle")
 
 
     #these if statements first check to see if all
     #sides are equal, then if two are, then if none are equal
 
-classify_triangle(0,0,0)
+def main():
+    classify_triangle(3,3,3)
+
+
+if __name__ == "__main__":
+    main()
 
